fc7ec44271996dba5b4dac2597a6f516f54ff6c1
Douglas Martins Santos <dougprogrammer@gmail.com>
2019-10-10 19:28:36 -0300

    configurando rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Test from '~/components/Test';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <Test />
-    </div>
-  );
-}
+const App = () => <Routes />;
 
 export default App;
